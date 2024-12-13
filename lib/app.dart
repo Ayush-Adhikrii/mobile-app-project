87a879feae68a08d99d5a00cdda1b8281c45d137
Ayush Adhikari <ayushadhikari64209@gmail.com>
2024-12-13 15:04:41 +0545

    get started page completed

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hooked/view/get_started.dart';
 import 'package:hooked/view/onboarding_view.dart';
 
 class MyApp extends StatelessWidget {
@@ -6,34 +7,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // return const MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   home: ColumnView(),
+    // );
     return MaterialApp(
       initialRoute: '/onboard',
       debugShowCheckedModeBanner: false,
       routes: {
         '/onboard': (context) => const OnboardingScreen(),
+        '/getstarted': (context) => const GetStarted(),
       },
-      theme: ThemeData(
-        primarySwatch:
-            Colors.pink, // You can adjust this as per your app's primary color
-        fontFamily: 'ProximaNova', // Set the global font to Proxima Nova
-        textTheme: const TextTheme(
-          bodyLarge: TextStyle(fontFamily: 'ProximaNova'),
-          bodyMedium: TextStyle(fontFamily: 'ProximaNova'),
-          bodySmall: TextStyle(fontFamily: 'ProximaNova'),
-          displayLarge: TextStyle(fontFamily: 'ProximaNova'),
-          displayMedium: TextStyle(fontFamily: 'ProximaNova'),
-          displaySmall: TextStyle(fontFamily: 'ProximaNova'),
-          headlineLarge: TextStyle(fontFamily: 'ProximaNova'),
-          headlineMedium: TextStyle(fontFamily: 'ProximaNova'),
-          headlineSmall: TextStyle(fontFamily: 'ProximaNova'),
-          titleLarge: TextStyle(fontFamily: 'ProximaNova'),
-          titleMedium: TextStyle(fontFamily: 'ProximaNova'),
-          titleSmall: TextStyle(fontFamily: 'ProximaNova'),
-          labelLarge: TextStyle(fontFamily: 'ProximaNova'),
-          labelMedium: TextStyle(fontFamily: 'ProximaNova'),
-          labelSmall: TextStyle(fontFamily: 'ProximaNova'),
-        ),
-      ),
     );
   }
 }
