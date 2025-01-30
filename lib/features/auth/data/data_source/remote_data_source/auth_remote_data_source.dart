809b0c9d1c3ae186c5878547c2c5ec3d438a7049
Ayush Adhikari <ayushadhikari64209@gmail.com>
2025-01-30 15:26:04 +0545

    use case made

---
+++ lib/features/auth/data/data_source/remote_data_source/auth_remote_data_source.dart
@@ -0,0 +1,118 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:softwarica_student_management_bloc/features/auth/data/data_source/auth_data_source.dart';
+import 'package:softwarica_student_management_bloc/features/auth/domain/entity/auth_entity.dart';
+
+import '../../../../../app/constants/api_endpoints.dart';
+
+class AuthRemoteDataSource implements IAuthDataSource {
+  final Dio _dio;
+
+  AuthRemoteDataSource(this._dio);
+
+  @override
+  Future<AuthEntity> getCurrentUser() {
+    return Future.value(AuthEntity(
+      userId: "",
+      name: "",
+      email: null,
+      phoneNumber: "",
+      userName: "",
+      password: "",
+      gender: null,
+      birthDate: null,
+      starSign: null,
+      bio: null,
+      profilePhoto: null,
+    ));
+  }
+
+  @override
+  Future<String> loginUser(String username, String password) async {
+    try {
+      Response response = await _dio.post(
+        ApiEndpoints.login,
+        data: {
+          "username": username,
+          "password": password,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        final str = response.data['token'];
+        return str;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } on DioException catch (e) {
+      throw Exception(e);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<void> registerUser(AuthEntity user) async {
+    try {
+      Response response = await _dio.post(
+        ApiEndpoints.register,
+        data: {
+          "name": user.name,
+          "gender": user.gender,
+          "email": user.email,
+          "birthDate": user.birthDate,
+          "starSign": user.starSign,
+          "bio": user.bio,
+          "phoneNumber": user.phoneNumber,
+          "username": user.userName,
+          "password": user.password,
+          "profilePhoto": user.profilePhoto,
+        },
+      );
+
+      if (response.statusCode == 200) {
+        return;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } on DioException catch (e) {
+      throw Exception(e);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+
+  @override
+  Future<String> uploadProfilePicture(File file) async {
+    try {
+      String fileName = file.path.split('/').last;
+      FormData formData = FormData.fromMap(
+        {
+          'profilePicture': await MultipartFile.fromFile(
+            file.path,
+            filename: fileName,
+          ),
+        },
+      );
+
+      Response response = await _dio.post(
+        ApiEndpoints.uploadImage,
+        data: formData,
+      );
+
+      if (response.statusCode == 200) {
+        // Extract the image name from the response
+        final str = response.data['data'];
+
+        return str;
+      } else {
+        throw Exception(response.statusMessage);
+      }
+    } on DioException catch (e) {
+      throw Exception(e);
+    } catch (e) {
+      throw Exception(e);
+    }
+  }
+}
