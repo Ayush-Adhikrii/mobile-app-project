3d05fcf7e7d1893223cf9193c8926238f3cebead
Ayush Adhikari <ayushadhikari64209@gmail.com>
2025-02-06 14:17:41 +0545

    added empty constructor

--- lib/features/auth/domain/entity/auth_entity.dart
+++ lib/features/auth/domain/entity/auth_entity.dart
@@ -5,7 +5,7 @@ class AuthEntity extends Equatable {
   final String name;
   final String? email;
   final String? phoneNumber;
-  final String userName; 
+  final String userName;
   final String password;
   final String? gender;
   final DateTime? birthDate;
@@ -26,6 +26,19 @@ class AuthEntity extends Equatable {
     this.bio,
     this.profilePhoto,
   });
+
+  const AuthEntity.empty()
+      : userId = 'empty_user_id',
+        name = 'empty_user_id',
+        email = 'empty_user_email',
+        phoneNumber = 'empty_user_phonenumber',
+        gender = 'empty_user_gender',
+        birthDate = null,
+        starSign = 'empty_user_starsign',
+        bio = 'empty_user_bio',
+        userName = 'empty_user_username',
+        password = 'empty_user_password',
+        profilePhoto = 'empty_user_photo';
 
   @override
   List<Object?> get props => [
