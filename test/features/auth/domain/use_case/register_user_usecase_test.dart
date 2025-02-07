a0225442f23dcd1e62f501735ad2bc622fa76001
Ayush Adhikari <ayushadhikari64209@gmail.com>
2025-02-07 20:59:25 +0545

    register testing

--- test/features/auth/domain/use_case/register_user_usecase_test.dart
+++ test/features/auth/domain/use_case/register_user_usecase_test.dart
@@ -5,7 +5,6 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:softwarica_student_management_bloc/features/auth/domain/entity/auth_entity.dart';
 import 'package:softwarica_student_management_bloc/features/auth/domain/use_case/register_user_usecase.dart';
-import 'package:softwarica_student_management_bloc/features/auth/domain/use_case/upload_image_usecase.dart';
 
 import 'repository.mock.dart';
 
@@ -14,23 +13,15 @@ class FakeFile extends Fake implements File {}
 void main() {
   late MockUserRepository repository;
   late RegisterUsecase usecase;
-  late UploadImageUsecase imageUsecase;
-
-  setUpAll(() {
-    registerFallbackValue(FakeFile()); // Registering a fallback value for File
-  });
 
   setUp(() {
     repository = MockUserRepository();
     usecase = RegisterUsecase(repository: repository);
-    imageUsecase = UploadImageUsecase(repository: repository);
-    registerFallbackValue(
-        AuthEntity.empty()); // Registering fallback for AuthEntity
+    registerFallbackValue(AuthEntity.empty());
   });
 
   final params = RegisterUserParams.empty();
-  final imageFile = File('/assets/images/profile.png');
-  final imageParams = UploadImageParams(file: imageFile);
+
   test('should register a user successfully', () async {
     when(() => repository.registerUser(any())).thenAnswer(
       (_) async => Right(unit),
@@ -44,22 +35,6 @@ void main() {
 
     // Verify
     verify(() => repository.registerUser(any())).called(1);
-    verifyNoMoreInteractions(repository);
-  });
-
-  test('should call upload image', () async {
-    when(() => repository.uploadProfilePicture(any())).thenAnswer(
-      (_) async => Right("image name"),
-    );
-
-    // Act
-    final result = await imageUsecase(imageParams);
-
-    // Assert
-    expect(result, Right("image name"));
-
-    // Verify
-    verify(() => repository.uploadProfilePicture(any())).called(1);
     verifyNoMoreInteractions(repository);
   });
 }
