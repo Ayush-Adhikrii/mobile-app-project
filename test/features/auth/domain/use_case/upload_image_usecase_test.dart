a879bd99d9d13eed119611ae56147a8b90c7dc68
Ayush Adhikari <ayushadhikari64209@gmail.com>
2025-02-07 20:59:43 +0545

    upload image testing

--- test/features/auth/domain/use_case/upload_image_usecase_test.dart
+++ test/features/auth/domain/use_case/upload_image_usecase_test.dart
@@ -1,7 +1,55 @@
+import 'dart:io';
+
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:softwarica_student_management_bloc/features/auth/domain/use_case/upload_image_usecase.dart';
+
+import 'register_user_usecase_test.dart';
+import 'repository.mock.dart';
 
 void main() {
-  testWidgets('upload image usecase ...', (tester) async {
-    // TODO: Implement test
+  late MockUserRepository repository;
+  late UploadImageUsecase imageUsecase;
+
+  setUp(() {
+    repository = MockUserRepository();
+    imageUsecase = UploadImageUsecase(repository: repository);
+    registerFallbackValue(FakeFile());
+  });
+
+  final imageFile = File('/assets/images/profile.png');
+  final imageParams = UploadImageParams(file: imageFile);
+
+  test('should call upload image', () async {
+    when(() => repository.uploadProfilePicture(any())).thenAnswer(
+      (_) async => Right("image name"),
+    );
+
+    // Act
+    final result = await imageUsecase(imageParams);
+
+    // Assert
+    expect(result, Right("image name"));
+
+    // Verify
+    verify(() => repository.uploadProfilePicture(any())).called(1);
+    verifyNoMoreInteractions(repository);
+  });
+
+  test('should call upload image', () async {
+    when(() => repository.uploadProfilePicture(any())).thenAnswer(
+      (_) async => Right("image name"),
+    );
+
+    // Act
+    final result = await imageUsecase(imageParams);
+
+    // Assert
+    expect(result, Right("photo name")); //photo instead of image
+
+    // Verify
+    verify(() => repository.uploadProfilePicture(any())).called(1);
+    verifyNoMoreInteractions(repository);
   });
 }
