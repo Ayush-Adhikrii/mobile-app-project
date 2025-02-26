8b74b52b1220e0ca2673945f3bd761433eb223fa
Ayush Adhikari <ayushadhikari64209@gmail.com>
2025-02-26 20:22:02 +0545

    user details

--- lib/features/user_details/presentation/view_model/bloc/user_details_state.dart
+++ lib/features/user_details/presentation/view_model/bloc/user_details_state.dart
@@ -1,28 +1,36 @@
-part of 'user_details_bloc.dart';
+// lib/features/user_details/presentation/view_model/bloc/user_details_state.dart
+import 'package:equatable/equatable.dart';
+import '../../../domain/entity/user_details_entity.dart';
 
-class UserDetailsState extends Equatable {
-  final bool isLoading;
-  final bool isSuccess;
-
-  const UserDetailsState({
-    required this.isLoading,
-    required this.isSuccess,
-  });
-
-  const UserDetailsState.initial()
-      : isLoading = false,
-        isSuccess = false;
-
-  UserDetailsState copyWith({
-    bool? isLoading,
-    bool? isSuccess,
-  }) {
-    return UserDetailsState(
-      isLoading: isLoading ?? this.isLoading,
-      isSuccess: isSuccess ?? this.isSuccess,
-    );
-  }
+abstract class UserDetailsState extends Equatable {
+  const UserDetailsState();
 
   @override
-  List<Object?> get props => [isLoading, isSuccess];
+  List<Object?> get props => [];
+}
+
+class UserDetailsInitial extends UserDetailsState {
+  const UserDetailsInitial();
+}
+
+class UserDetailsLoading extends UserDetailsState {
+  const UserDetailsLoading();
+}
+
+class UserDetailsLoaded extends UserDetailsState {
+  final UserDetailsEntity userDetails;
+
+  const UserDetailsLoaded(this.userDetails);
+
+  @override
+  List<Object?> get props => [userDetails];
+}
+
+class UserDetailsError extends UserDetailsState {
+  final String message;
+
+  const UserDetailsError(this.message);
+
+  @override
+  List<Object?> get props => [message];
 }
