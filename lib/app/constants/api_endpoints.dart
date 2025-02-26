dad4c320a1c7ec121219d596b6cffde868687772
Ayush Adhikari <ayushadhikari64209@gmail.com>
2025-02-26 20:21:12 +0545

    url modified

--- lib/app/constants/api_endpoints.dart
+++ lib/app/constants/api_endpoints.dart
@@ -5,21 +5,21 @@ class ApiEndpoints {
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
 
-  // Define the IP address (easier to change here)
   static const String ipAddress = "192.168.1.69";
-
-  // Base URL for API requests
-  static String get baseUrl => "http://$ipAddress:5000/api/";
-
-  // Image URL for uploads and access
-  static String get imageUrl => "http://$ipAddress:5000/uploads/";
-
+  static String get baseUrl => "http://$ipAddress:5000/api/"; // Backend API
+  static String get profilePhotoUrl => "http://$ipAddress:5173/profilePhotos/"; // Frontend server
+  static String get userImageUrl => "http://$ipAddress:5173/userImages/"; // Frontend server
   // ====================== Auth Routes ======================
-  static const String login = "user/login";
-  static const String register = "user";
-  static const String uploadImage = "user/uploadImage";
+  static const String login = "auth/login";
+  static const String currentUser = "auth/me";
+  static const String register = "auth/signup";
+  static const String uploadImage = "auth/uploadImage";
 
   // ====================== User Details Routes ======================
-  static const String addUserDetails = "details";
-  static const String getUserDetails = "details/getUserDetails";
+  static const String addUserDetails = "userDetails/add";
+  static const String updateUserDetails = "userDetails/";
+  static const String getUserDetails = "userDetails/user";
+
+  // ====================== Match Routes ======================
+  static const String getUsers = "/matches/user-profiles";
 }
