6cbdbf2ec6fe5f8aa6e2f737c54e39511f58f52e
Ayush Adhikari <ayushadhikari64209@gmail.com>
2025-03-03 21:55:22 +0545

    added other icons for navigation

--- lib/features/home/presentation/view/home_view.dart
+++ lib/features/home/presentation/view/home_view.dart
@@ -1,77 +1,43 @@
+// lib/features/home/presentation/view/home_view.dart
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:softwarica_student_management_bloc/core/common/snackbar/my_snackbar.dart';
-import 'package:softwarica_student_management_bloc/features/home/presentation/view_model/home_cubit.dart';
-import 'package:softwarica_student_management_bloc/features/home/presentation/view_model/home_state.dart';
+import 'package:softwarica_student_management_bloc/app/di/di.dart';
+import '../view_model/home_cubit.dart';
+import '../view_model/home_state.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
-  final bool _isDarkTheme = false;
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            icon: const Icon(Icons.logout),
-            onPressed: () {
-              // Logout code
-              showMySnackBar(
-                context: context,
-                message: 'Logging out...',
-                color: Colors.red,
-              );
-
-                context.read<HomeCubit>().logout(context);
-            },
-          ),
-          Switch(
-            value: _isDarkTheme,
-            onChanged: (value) {
-              // Change theme
-              // setState(() {
-              //   _isDarkTheme = value;
-              // });
-            },
-          ),
-        ],
-      ),
-      // body: _views.elementAt(_selectedIndex),
-      body: BlocBuilder<HomeCubit, HomeState>(builder: (context, state) {
-        return state.views.elementAt(state.selectedIndex);
-      }),
-      bottomNavigationBar: BlocBuilder<HomeCubit, HomeState>(
-        builder: (context, state) {
-          return BottomNavigationBar(
-            items: const <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.dashboard),
-                label: 'Dashboard',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.book),
-                label: 'Course',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.group),
-                label: 'Batch',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.account_circle),
-                label: 'Account',
-              ),
-            ],
-            currentIndex: state.selectedIndex,
-            selectedItemColor: Colors.white,
-            onTap: (index) {
-              context.read<HomeCubit>().onTabTapped(index);
-            },
-          );
-        },
+    return BlocProvider(
+      create: (context) => getIt<HomeCubit>(),
+      child: Scaffold(
+        body: BlocBuilder<HomeCubit, HomeState>(
+          builder: (context, state) {
+            return state.views.elementAt(state.selectedIndex);
+          },
+        ),
+        bottomNavigationBar: BlocBuilder<HomeCubit, HomeState>(
+          builder: (context, state) {
+            return BottomNavigationBar(
+              items: const [
+                BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ''),
+                BottomNavigationBarItem(icon: Icon(Icons.filter_list), label: ''),
+                BottomNavigationBarItem(icon: Icon(Icons.people), label: ''),
+                BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+                BottomNavigationBarItem(icon: Icon(Icons.message), label: ''),
+              ],
+              currentIndex: state.selectedIndex,
+              selectedItemColor: Colors.white,
+              unselectedItemColor: Colors.grey,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              type: BottomNavigationBarType.fixed,
+              onTap: (index) => context.read<HomeCubit>().onTabTapped(index),
+            );
+          },
+        ),
       ),
     );
   }
